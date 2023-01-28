72a8fc3d8628dced02041ee995d36dda2ae3201c
Lewis Moore <lewismooredev@gmail.com>
2023-01-28 15:37:20 +0000

    add functionalirtry o increment seconds at 100ms

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let interval;
 const startTimer = () => {
     let timerIncremement = document.getElementById('tensTimer')
     timerIncremement.innerHTML ++
+    isTensTimerOverOneHundred(timerIncremement)
 }
 
 const startButton = () => {
@@ -20,3 +21,17 @@ const resetTimer = () => {
     document.getElementById('tensTimer').innerHTML = '00'
     document.getElementById('secondsTimer').innerHTML = '00'
 }
+
+const incrementSeconds = () => {
+    document.getElementById('secondsTimer').innerHTML++ 
+    document.getElementById('tensTimer').innerHTML = '00'
+}
+
+const isTensTimerOverOneHundred = (timerIncremement) => {
+    console.log('In tens check')
+    if (timerIncremement.innerHTML >= 100) {
+        incrementSeconds()
+    }
+}
+
+
